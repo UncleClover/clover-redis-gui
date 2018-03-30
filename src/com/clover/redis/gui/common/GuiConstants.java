3e63cfa0786b71fe8c25888837a288b2fd40ac4e
zhangdq <zhangdq@asiainfo.com>
2018-03-30 22:11:10 +0800

    初始项目

---
+++ src/com/clover/redis/gui/common/GuiConstants.java
@@ -0,0 +1,5 @@
+package com.clover.redis.gui.common;
+
+public class GuiConstants {
+	
+}
---
+++ src/com/clover/redis/gui/test/Test.java
@@ -0,0 +1,7 @@
+package com.clover.redis.gui.test;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Hello Redis Gui!");
+	}
+}
