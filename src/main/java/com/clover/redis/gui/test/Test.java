d261e5e89ec7ccd0a4817d39ee2352d2b23d1de2
UncleClover <qiang900714@126.com>
2018-04-02 22:24:26 +0800

    增加mainframe

--- src/main/java/com/clover/redis/gui/test/Test.java
+++ src/main/java/com/clover/redis/gui/test/Test.java
@@ -4,10 +4,12 @@ import redis.clients.jedis.Jedis;
 
 public class Test {
 	public static void main(String[] args) {
+		@SuppressWarnings("resource")
 		Jedis jedis = new Jedis("127.0.0.1", 16379);
 		jedis.auth("zhangdq");
 		System.out.println(jedis.ping());
-		jedis.select(1);
+		jedis.select(11);
 		System.out.println(jedis.keys("*"));
+		System.out.println(jedis.getDB());
 	}
 }
